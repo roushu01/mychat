fdf69d418a27700a6b1894e6162083e0c1a2d6d3
roushu01 <142681156+roushu01@users.noreply.github.com>
2025-04-23 23:06:26 +0530

    Update App.js

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io("http://localhost:3000");
+const socket = io("https://mychat-1-8j7s.onrender.com");
 
 function App() {
   const [message, setMessage] = useState('');
