b167a2743aab72590cc3e6b85fdf383de0c4324d
roushu01 <roushnikumar@gmail.com>
2025-04-23 22:39:49 +0530

    first commit

---
+++ chat-backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('sendMessage', (message) => {
+    const timestamped = { ...message, timestamp: new Date() };
+    io.emit('receiveMessage', timestamped);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
